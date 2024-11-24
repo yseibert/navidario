67fbd094b43bd12fbe9217e58182dd10498123dc
Yannic Seibert <ge59tus@mytum.de>
2024-11-24 13:21:46 +0100

    add snow background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<header>
+    <h1>Ortenisa's Navidario</h1>
+</header>
+
+<style>
+    header {
+        text-align: center;
+    }
+
+    h1 {
+        color: white
+    }
+</style>
